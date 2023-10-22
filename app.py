43e1597192cf5dd2c20220b8c62019edc8cc70a3
Chad Bercea <chadbercea@gmail.com>
2023-10-22 15:03:10 -0400

    Flask Chaching module integration
    I'm also going to just push this and see if the heroku auto deploying thing I set up works. If this appears at the .com then holy f bomb bat man I'm an automation Gandalf where's my pipeweed?

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_caching import Cache
 
 app = Flask(__name__)
 
+# Configuring Flask-Caching
+cache_config = {
+    "CACHE_TYPE": "simple", # Simple in-memory cache
+    "CACHE_DEFAULT_TIMEOUT": 300 # Cache timeout: 5 minutes
+}
+app.config.from_mapping(cache_config)
+
+cache = Cache(app)
+
 @app.route('/')
 def hello_world():
-    return 'Will I Donate My Kit? We help you calculate the odds.'
+    return 'Will I Donate My Kit? We help you calculate the odds. First auto deploy. #LFG'
